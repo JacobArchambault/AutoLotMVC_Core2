2ebb367fccd44a8cc9e8932ced3aefabc3da5264
Jacob Archambault <jacob.archambault@waystar.com>
2020-04-24 13:59:51 -0400

    Add DataInitialization support to Program's Main method. Causes runtime error 'No service for type AutoLotContext has been registered'

--- AutoLotMVC_Core2/Program.cs
+++ AutoLotMVC_Core2/Program.cs
@@ -6,6 +6,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using AutoLotDAL_Core2.DataInitialization;
+using AutoLotDAL_Core2.EF;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore;
 
 namespace AutoLotMVC_Core2
 {
@@ -13,14 +17,19 @@ namespace AutoLotMVC_Core2
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var webHost = BuildWebHost(args);
+            using (var scope = webHost.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<AutoLotContext>();
+                MyDataInitializer.RecreateDatabase(context);
+                MyDataInitializer.InitializeData(context);
+            }
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>()
+            .Build();
     }
 }
